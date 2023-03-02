9d8dac15bc5d4cd9ee425cb589a9a78d25681a57
noor hadi <h27995824@gmail.com>
2023-03-02 03:55:06 +0300

    first commit

---
+++ allData.js
@@ -0,0 +1,13 @@
+const  firstName = "ahmed"
+const  lastName = "mohamed"
+const  city = "hilla"
+const  age = 45
+
+
+module.exports = {
+    fname : firstName,
+    lname : lastName,
+    city : city,
+    age : age,
+    
+}
